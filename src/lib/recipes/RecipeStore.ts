cc0ed29e783bc2780b81992314766874a2658848
Krutoy242 <Krutoy242@gmail.com>
2022-04-27 08:37:15 +0300

    feat(calc): 📈 Merge same inputs and outputs to catals

--- src/lib/recipes/RecipeStore.ts
+++ src/lib/recipes/RecipeStore.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 import { createFileLogger } from '../../log/logger'
 import DefinitionStore from '../items/DefinitionStore'
 import Ingredient from '../items/Ingredient'
@@ -16,6 +18,8 @@ type RecipeParams = [
   catalysts?: AnyIngredients
 ]
 
+type RecipeLists = [outputs: Stack[], inputs?: Stack[], catalysts?: Stack[]]
+
 export default class RecipeStore {
   store: Recipe[] = []
 
@@ -29,10 +33,6 @@ export default class RecipeStore {
     return this.store.map((r) => r.export())
   }
 
-  forCategory(categoryName: string) {
-    return (...params: RecipeParams) => this.addRecipe(categoryName, ...params)
-  }
-
   addRecipe(categoryName: string, ...params: RecipeParams): boolean {
     const [outputs, inputs, catalysts] = params.map((p) =>
       this.parseRecipeParams(p)
@@ -44,10 +44,49 @@ export default class RecipeStore {
       return false
     }
 
-    const recipe = new Recipe(categoryName, outputs, inputs, catalysts)
+    const recipeLists = this.uniformCatalysts(outputs, inputs, catalysts)
+
+    const recipe = new Recipe(categoryName, ...recipeLists)
     recipe.index = this.store.push(recipe) - 1
 
     return true
+  }
+
+  uniformCatalysts(...args: RecipeLists): RecipeLists {
+    if (args[1]?.length === 0) return args
+
+    const outputs = args[0].slice(0)
+    const inputs = args[1]?.slice(0) ?? []
+    const catalList = args[2]?.slice(0) ?? []
+    let hadChanges = false
+    for (let out_i = outputs.length - 1; out_i >= 0; out_i--) {
+      const out = outputs[out_i]
+      const index = inputs.findIndex(
+        (s) => s.ingredient.id === out.ingredient.id
+      )
+
+      if (index === -1) continue // No intersection
+      const inp = inputs[index]
+
+      const out_amount = out.amount ?? 1
+      const inp_amount = inp.amount ?? 1
+      if (out_amount < inp_amount) continue // Non-benefit
+
+      if (out_amount > inp_amount) {
+        outputs[out_i] = out.withAmount(out_amount - inp_amount)
+      } else {
+        // amounts equals
+        outputs.splice(out_i, 1)
+      }
+      inputs.splice(index, 1)
+      catalList.push(inp)
+
+      hadChanges = true
+    }
+
+    if (!hadChanges || outputs.length === 0) return args
+
+    return [outputs, inputs, catalList]
   }
 
   private anyRecipeParam(anyIngrs: AnyIngredient): Stack {
